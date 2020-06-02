fd8c291a2b6d6727849fb3dbd9ee76586971077f
Piyush Jiwane <jiwanepiyush@gmail.com>
2020-06-02 13:43:52 +0530

    added

---
+++ repos/CustomeCellApp/CustomeCellApp/CustomeCellApp/Custome.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomeCellApp
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public string ImageUrl { get; set; }
+    }
+
+}
---
+++ repos/CustomeCellApp/CustomeCellApp/CustomeCellApp/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CustomeCellApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            listName.ItemsSource = new List<Contact> {
+                        new Contact{Name="Piyush Jiwane", ImageUrl = "arrow.png" , Status="Single"},
+                        new Contact{Name="Gaurav Jiwane", ImageUrl = "arrow1.png" ,  Status="only calls, no whatsapp"},
+                };
+        }
+
+    }
+}
